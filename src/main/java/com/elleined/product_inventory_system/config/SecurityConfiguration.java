ab24044c83f3a5718d74a640887df98a4c36707c
elleined <demadegu@gmail.com>
2024-11-27 22:33:13 +0800

    Added oauth2 login

---
+++ src/main/java/com/elleined/product_inventory_system/config/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.elleined.product_inventory_system.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+
+                .oauth2Login(oc -> oc
+                        .defaultSuccessUrl("/home"))
+
+                .authorizeHttpRequests(c -> c.anyRequest().permitAll())
+                .build();
+    }
+}
